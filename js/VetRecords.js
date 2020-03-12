4e75a2692de9b21cac89398fc057f612b49a5bb4
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-11 20:49:36 -0500

    done for the night

--- js/VetRecords.js
+++ js/VetRecords.js
@@ -1,5 +1,12 @@
 "use strict";
 
 //Assigning a pet name
-let petname = $("#newnamediv").value;
-$("#newinfosubmit").onclick.html().append(petname);
+let newpetname = $("#nameinput").val();
+
+//This part works, it's the let that doesn't
+$("#newinfosubmit").click(function () {
+    console.log("Running");
+    console.log(newpetname);
+    $("#namediv").html("Name: ").append(newpetname);
+    }
+);
