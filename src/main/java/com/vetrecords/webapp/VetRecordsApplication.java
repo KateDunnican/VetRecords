52cc3efa089d37f3c9e7c61bafd0c4846f8abfe1
Kate McKinney <kate.mckinney210@gmail.com>
2020-06-15 10:43:49 -0500

    setup as spring project

---
+++ src/main/java/com/vetrecords/webapp/VetRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.vetrecords.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetrecords/webapp/VetRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetrecords.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
